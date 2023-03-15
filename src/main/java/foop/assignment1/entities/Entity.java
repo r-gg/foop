4d5abfd3b6ede4118afb5c4afde0e110bafe7e5e
Rastko Gajanin <e11930500@student.tuwien.ac.at>
2023-03-15 18:55:59 +0100

    Add base

---
+++ src/main/java/foop/assignment1/entities/Entity.java
@@ -0,0 +1,12 @@
+package foop.assignment1.entities;
+
+public abstract class Entity {
+
+    protected float x,y;
+    public Entity(float x, float y) {
+        this.x = x;
+        this.y = y;
+
+    }
+
+}
